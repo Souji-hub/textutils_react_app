68c8938c37ce8e000b278456574ae8e5192cbca6
Souji-hub <idebussoujan@gmail.com>
2024-12-05 12:42:36 +0545

    added functionalities in app

---
+++ src/components/About.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="container">
+      <h2 className="my-2"> About Us</h2>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              About TextUtils
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>The TextUtils app</strong> is a handy tool for
+              manipulating text efficiently. It allows you to perform operations
+              like converting text to uppercase or lowercase, removing extra
+              spaces, and counting words or characters. These features make it
+              ideal for writers, developers, and anyone working with text. You
+              can also customize the behavior to suit your needs using
+              additional settings. The app is versatile and can process various
+              text-based tasks, such as analyzing input or preparing clean data
+              for projects. It's a simple yet powerful solution for text-related
+              challenges. All of this functionality is achieved through
+              intuitive features and user-friendly design, ensuring a seamless
+              experience for users.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              About the Programmer
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>Soujanya Subedi</strong> is a talented programmer
+              currently pursuing a Master's degree in Computer Engineering. With
+              a strong foundation in Electronics and Communication Engineering,
+              Soujanya has worked as a robotics engineer for two years,
+              showcasing expertise in innovative technologies and
+              problem-solving. The combination of technical skills and a passion
+              for programming makes Soujanya adept at tackling complex
+              challenges, especially in fields like robotics, software
+              development, and system design. Always eager to learn and grow,
+              Soujanya leverages a unique blend of analytical thinking and
+              creativity to deliver impactful solutions in the tech world.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              About React JS
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>React JS</strong> is a powerful and flexible JavaScript
+              library used for building user interfaces, particularly
+              single-page applications. Developed by Facebook, React enables
+              developers to create reusable UI components, manage application
+              state efficiently, and ensure a seamless user experience. By
+              utilizing a virtual DOM, React optimizes rendering performance,
+              making it a popular choice for high-performance applications. Its
+              component-based architecture encourages modular development,
+              enhancing code readability and maintainability. React's ecosystem
+              is vast, including tools like React Router for navigation and
+              state management libraries like Redux. Whether you're building
+              dynamic web apps or mobile apps with React Native, this library
+              provides a robust foundation for modern development.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+  const [numbers, setNumbers] = useState("");
+
+  let wordCount = text.split(" ").length;
+
+  //very imp: when using forms, textarea, remember it
+  const handleOnChange = (event) => {
+    console.log("onChange Event");
+    setText(event.target.value);
+  };
+
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked. Value: " + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLowClick = () => {
+    console.log("Lowercase was clicked. Value: " + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleNumberExtraction = () => {
+    console.log("Numbers Extracted");
+    let extractedNumbers = text.split(/[A-Za-z]/);
+    setNumbers(extractedNumbers); //using regex
+  };
+
+  const handleClearText = () => {
+    console.log("Text is cleared");
+    setText("");
+    setNumbers("");
+  };
+  return (
+    <>
+      <div className="container my-2">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myTextBox"
+            rows="8"
+            placeholder="Enter Text Here"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+            Convert to Uppercase
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+            Convert to Lowercase
+          </button>
+          <button
+            className="btn btn-primary mx-2"
+            onClick={handleNumberExtraction}
+          >
+            Extract Numbers
+          </button>
+          <button className="btn btn-primary mx-2" onClick={handleClearText}>
+            Clear
+          </button>
+        </div>
+      </div>
+      <div className="container my-2">
+        <h2>Your Text Summary</h2>
+        <p>
+          {wordCount} words, {text.length} characters in your text.
+        </p>
+        {/* acc to net,  avg reading time is 125 words per minute */}
+        <p>
+          {" "}
+          {((1 / 125) * wordCount).toFixed(2)} minutes reading time for above
+          text.
+        </p>
+        <h3>Extracted Numbers</h3>
+        <p>{numbers.length > 0 ? numbers : "None"}</p>
+        <h3>Preview</h3>
+        <p>{text.length > 0 ? text : "Nothing to Show"}</p>
+      </div>
+    </>
+  );
+}
